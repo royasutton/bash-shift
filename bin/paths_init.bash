d8a60db5d2484ea951be2847a45c12179f863974
Roy Allen Sutton <royasutton@hotmail.com>
2024-11-17 11:19:10 -0600

    Add common file name initialization.

---
+++ bin/paths_init.bash
@@ -0,0 +1,67 @@
+###############################################################################
+#
+# Common directory and file name initialization.
+#
+###############################################################################
+
+#
+# install prefix and configuration path mode {0: absolute, 1:relative}
+#
+declare script_home_prefix=${HOME}/local
+declare script_path_mode=1
+
+#
+# verify minimum bash version
+#
+if [ $BASH_VERSINFO -lt 4 ] ; then
+    echo "bash version is $BASH_VERSION. version 4 or greater required."
+    exit 1
+fi
+
+#
+# script name and location
+#
+declare script_base_path="${0%/*}"
+declare script_base_name="${0##*/}"
+declare script_root_name="${script_base_name%.*}"
+
+#
+# library and configuration directories
+#
+declare script_lib_dir
+declare script_conf_dir
+
+if [ "${script_path_mode}" == "0" ] ; then
+  # absolute directory path
+  script_lib_dir="${script_home_prefix}/lib"
+
+  if [ -d "${script_home_prefix}/etc/${script_root_name}" ] ; then
+    script_conf_dir="${script_home_prefix}/etc/${script_root_name}"
+  else
+    script_conf_dir="${script_home_prefix}/etc"
+  fi
+else
+  # directory path relative to script path
+  script_lib_dir="$(cd ${script_base_path}/../lib; pwd)"
+
+  if [ -d "${script_base_path}/../etc/${script_root_name}" ] ; then
+    script_conf_dir="$(cd ${script_base_path}/../etc/${script_root_name}; pwd)"
+  else
+    script_conf_dir="$(cd ${script_base_path}/../etc; pwd)"
+  fi
+fi
+
+#
+# default configuration filename
+#
+declare script_conf_file="${script_conf_dir}/${script_root_name}.conf"
+
+#
+# default temporary-file root name
+#
+declare script_tmp_root="/tmp/${script_root_name}-$$"
+
+
+###############################################################################
+# eof
+###############################################################################
